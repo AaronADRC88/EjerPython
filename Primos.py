9393a5702f97b6195ea4fa821077b0aed4b2d6ac
aferreiradominguez <aferreiradominguez@atanasoff07.danielcastelao.org>
2015-10-13 11:42:00 +0200

    primos ok

--- Primos.py
+++ Primos.py
@@ -1,36 +1,19 @@
 __author__ = 'aferreiradominguez'
-#N?meros primos
-
-def es_primo(num):
-    if num < 2:
-    #si es menos que 2 no es primo, por lo tanto devolver? Falso
-        return False
-    for i in range(2, num):
-    #un rango desde el dos hasta el numero que nosotros elijamos
-        if num % i == 0:
-        #si el resto da 0 no es primo, por lo tanto devuelve Falso
-            return False
-    return True
-    #de lo contrario devuelve Verdadero
-
-print es_primo(3)
-#para probarlo llamamos a la funci?n
+s = int(input("Introduce numero:"))
+print(1)
 
 
+def primo(j):
+    i = 2
+    while i < j:
+        resto = j % i
+        if (resto == 0):
+            return 0
+        i = i + 1
+    print(str(j))
 
-def primos(num1, num2):
 
-    cont = 0
-
-    for i in range(num1, num2):
-        if es_primo(i) == True:
-        #Llamamos a la primera funcion para ahorrar trabajo ;)
-            cont += 1
-            #Que va a determinar si es primo o no
-                print i
-
-    print ""
-    print "Hay", cont, "numeros primos"
-    #Total de numeros primos
-
-print primos(487, 8976)
+j = 2
+while j < s:
+    a = primo(j)
+    j = j + 1
